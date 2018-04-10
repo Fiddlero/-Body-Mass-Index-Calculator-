841e59fb6479adb6a64ae7ad2ad8c5d2d8409360
Skrzypek <bartosz.sk@op.pl>
2018-04-10 11:13:43 +0200

    Version 1.1, added graphical interface

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,14 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("BMI_Calculator.fxml"));
+        Scene scene=new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
 
